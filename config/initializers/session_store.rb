8c3c1dffb15f06f3285eced14fce9bcb3a94c106
zeknox <mccann.brandon@gmail.com>
2013-10-16 16:54:28 -0500

    fixed #8 issue with 2 hour expiration

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-PhishingFramework::Application.config.session_store :cookie_store, key: '_phishing-framework_session'
+PhishingFramework::Application.config.session_store :cookie_store, key: '_phishing-framework_session', :expire_after => 120.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
