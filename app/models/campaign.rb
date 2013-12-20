309499e16a65d6d9a71ab6c84ad72fc4c606881a
Adam Dalton <adalton@ihmc.us>
2013-12-20 11:30:29 -0500

    (un)deploy campaigns based on active status

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,120 +1,135 @@
+require 'fileutils'
+
 class Campaign < ActiveRecord::Base
-	# relationships
-	belongs_to :template
-	has_one :campaign_settings, dependent: :destroy
-	has_one :email_settings, dependent: :destroy
-	has_many :statistics
-	has_many :victims
-	has_many :smtp_communications
+  # relationships
+  belongs_to :template
+  has_one :campaign_settings, dependent: :destroy
+  has_one :email_settings, dependent: :destroy
+  has_many :statistics
+  has_many :victims
+  has_many :smtp_communications
   has_many :blasts
 
-	# allow mass asignment
-	attr_accessible :name, :description, :active, :emails, :scope, :template_id, :test_email
+  # allow mass asignment
+  attr_accessible :name, :description, :active, :emails, :scope, :template_id, :test_email
 
-	# named scopes
-	scope :active, where(:active => true)
-	scope :launched, where(:email_sent => true)
+  # named scopes
+  scope :active, where(:active => true)
+  scope :launched, where(:email_sent => true)
 
-	before_save :parse_email_addresses
-	after_save :check_changes
+  before_save :parse_email_addresses
+  after_save :check_changes
+  after_update :devops, :if => :active_changed?
 
-	# validate form before saving
-	validates :name, :presence => true,
-		:length => { :maximum => 255 }
-	validates :description,
-		:length => { :maximum => 255 }
-	validates :emails,
-		:length => { :maximum => 60000 }
-	validates :scope, :numericality => { :greater_than_or_equal_to => 0 },
-		:length => { :maximum => 4 }, :allow_nil => true
+  # validate form before saving
+  validates :name, :presence => true,
+            :length => {:maximum => 255}
+  validates :description,
+            :length => {:maximum => 255}
+  validates :emails,
+            :length => {:maximum => 60000}
+  validates :scope, :numericality => {:greater_than_or_equal_to => 0},
+            :length => {:maximum => 4}, :allow_nil => true
 
 
-	def test_victim
-		v = Victim.new
-		v.email_address = test_email
-		v
-	end
+  def test_victim
+    v = Victim.new
+    v.email_address = test_email
+    v
+  end
 
-	private
+  private
 
-	def parse_email_addresses
-		if not self.emails.blank?
-			# csv or carriage
-			if self.emails.include? ","
-				victims = self.emails.split(",")
-				victims.each do |v|
-					victim = Victim.new
-					victim.campaign_id = self.id
-					victim.email_address = v.strip
-					victim.save
-				end
-			else
-				victims = self.emails.split("\r\n")
-				victims.each do |v|
-					victim = Victim.new
-					victim.campaign_id = self.id
-					victim.email_address = v
-					victim.save
-				end
-			end
+  def parse_email_addresses
+    if not self.emails.blank?
+      # csv or carriage
+      if self.emails.include? ","
+        victims = self.emails.split(",")
+        victims.each do |v|
+          victim = Victim.new
+          victim.campaign_id = self.id
+          victim.email_address = v.strip
+          victim.save
+        end
+      else
+        victims = self.emails.split("\r\n")
+        victims.each do |v|
+          victim = Victim.new
+          victim.campaign_id = self.id
+          victim.email_address = v
+          victim.save
+        end
+      end
 
-			# clear the Campaigns.emails holder
-			self.update_attribute(:emails, " ")
-		end
-	end
+      # clear the Campaigns.emails holder
+      self.update_attribute(:emails, " ")
+    end
+  end
 
-	def check_changes
-		if campaign_settings == nil
-			return false
-		end
+  def check_changes
+    Rails.logger.info "!!!!CHECKING CHANGES!!!!"
+    if campaign_settings == nil
+      return false
+    end
 
-		if self.changed?
-			httpd = GlobalSettings.first.path_apache_httpd
-			template = Template.find_by_id(self.template_id)
+    if self.changed?
+      Rails.logger.info "!!!!CHANGED!!!!"
+      httpd = GlobalSettings.first.path_apache_httpd
+      template = Template.find_by_id(self.template_id)
 
-			# gather active campaigns
-			active_campaigns = Campaign.active
+      # gather active campaigns
+      active_campaigns = Campaign.active
 
-			# flush httpd config
-			File.open(httpd, 'w') {|file| file.truncate(0) }
+      # flush httpd config
+      File.open(httpd, 'w') { |file| file.truncate(0) }
 
-			# write each active campaign to httpd
-			active_campaigns.each do |campaign|
-				File.open(httpd, "a+") do |f|
-					template = Template.find_by_id(campaign.template_id)
-					if template.nil?
-						raise 'Template #{campaign.template_id} not found'
+      # write each active campaign to httpd
+      active_campaigns.each do |campaign|
+        File.open(httpd, "a+") do |f|
+          template = Template.find_by_id(campaign.template_id)
+          if template.nil?
+            raise 'Template #{campaign.template_id} not found'
 
-					else
-						f.write(vhost_text(campaign.id, campaign.campaign_settings.fqdn, template.location))
-					end
-				end
-			end
+          else
+            f.write(vhost_text)
+          end
+        end
+      end
 
-			# reload apache
-			restart_apache = GlobalSettings.first.command_apache_restart
-			system(restart_apache)
-		end
-	end
+      # reload apache
+      restart_apache = GlobalSettings.first.command_apache_restart
+      Rails.logger.info system(restart_apache)
+    end
+  end
 
-	def vhost_text(campaign_id, fqdn, template_location)
-		approot = Rails.root
+  def vhost_text
+    @campaign_id = id
+    @fqdn = campaign_settings.fqdn
+    @template_location = deployment_directory
+    @approot = Rails.root
+    template = ERB.new File.read(File.join(Rails.root, "app/views/campaigns/virtual_host.txt.erb", ))
+    template.result(binding)
+  end
 
-		vhost_text = <<-VHOST
-			# #{fqdn}
-			<VirtualHost *:80>
-							ServerName #{fqdn}
-							ServerAlias #{fqdn}
-							ServerAlias www.#{fqdn}
-							DocumentRoot #{approot}/public/templates/#{template_location}/www/
-							DirectoryIndex index.php
-							CustomLog     #{approot}/log/www-#{fqdn}-#{campaign_id}-access.log combined
-							ErrorLog      #{approot}/log/www-#{fqdn}-#{campaign_id}-error.log
-			</VirtualHost>
 
-		VHOST
 
-		return vhost_text
-	end
+  def devops
+    active ? deploy : undeploy
+  end
+
+  def undeploy
+    FileUtils.rm_rf deployment_directory
+  end
+
+  def deploy
+    FileUtils.mkdir_p(deployment_directory)
+    template.website_files.each do |page|
+      FileUtils.cp(page.file.current_path, File.join(deployment_directory, page[:file]))
+    end
+  end
+
+  def deployment_directory
+    File.join(Rails.root, "public/deployed/campaigns", id.to_s)
+  end
 
 end
