264228002865a8a9c86ba37480738303e233c4ea
zeknox <mccann.brandon@gmail.com>
2014-01-27 21:57:36 -0600

    removed template creation since template:load task takes care of this now

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,6 @@ connection = ActiveRecord::Base.connection();
 unless ENV['SEED_DATA'] == 'devise'
   connection.execute("INSERT INTO admins ( name, username, password, salt, active, created_at, updated_at ) VALUES ( 'admin', 'admin', '25290094b52f4dd1af5be269481a9de412590a0d', 'c2e395561aeaee9160d0d49196aad9d9bd449a9f', '1', '2013-01-01', '2013-01-01')")
 
-# Create intial phishing templates
-Template.new(name: 'Intel Password Checker', location: 'intel', description: 'Users test the strength of their password').save(validate: false)
-Template.new(name: 'Efax', location: 'efax', description: 'User received a efax which requires them to open the PDF').save(validate: false)
-
 # Create Default Global Settings
   GlobalSettings.create({
                             command_apache_restart: 'sudo /etc/init.d/apache2 reload',
@@ -20,7 +16,6 @@ Template.new(name: 'Efax', location: 'efax', description: 'User received a efax 
                         })
 
 end
-
 
 connection.execute("UPDATE admins SET encrypted_password = '$2a$10$vNos/nCiOItiqDhSKmqO..OoxCLEck.sLtny1mXIAN2vkOjwB.kmG' WHERE admins.id = 1")
 admin = Admin.first
