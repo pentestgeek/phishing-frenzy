d32a6cf3a618b73193bb275cf1a2c19e8839f2fa
drag00n <christophermluciano@gmail.com>
2014-06-29 19:39:34 -0400

    initial cookbook

---
+++ cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name             'phishing-frenzy'
+maintainer       'CHristopher M Luciano'
+maintainer_email 'christophermluciano@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures phishing-frenzy'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
