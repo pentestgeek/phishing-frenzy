1104fe0137d15407a0ac30bfa53557652160a684
zeknox <mccann.brandon@gmail.com>
2014-07-11 00:09:20 -0500

    merged in SMTP Delay option from Alton, thanks bro!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140506021435) do
+ActiveRecord::Schema.define(:version => 20140706224849) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(:version => 20140506021435) do
     t.boolean  "use_beef",               :default => false
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
+    t.integer  "smtp_delay",             :default => 0
   end
 
   add_index "campaign_settings", ["campaign_id"], :name => "index_campaign_settings_on_campaign_id"
@@ -101,6 +102,15 @@ ActiveRecord::Schema.define(:version => 20140506021435) do
   end
 
   add_index "campaigns", ["template_id"], :name => "index_campaigns_on_template_id"
+
+  create_table "clones", :force => true do |t|
+    t.string   "name"
+    t.string   "status"
+    t.text     "url"
+    t.text     "page"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "email_settings", :force => true do |t|
     t.integer  "campaign_id"
