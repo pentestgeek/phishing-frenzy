366f034427650b77f810bbdfb06a7f667dd1c77a
drag00n <christophermluciano@gmail.com>
2014-07-24 00:17:18 -0400

    preliminary initial release

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -6,3 +6,9 @@ description      'Installs/Configures phishing-frenzy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends "apt"
+depends "rbenv"
+depends "apache"
+depends "passenger_apache2"
+depends "mysql"
+depends "database"
