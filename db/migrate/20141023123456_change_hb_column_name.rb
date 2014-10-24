a156289b1006fb4432f944956fd9c2d73b5bb648
antisnatchor <antisnatchor@gmail.com>
2014-10-24 12:57:05 +0200

    Remote unneeded migrate

--- db/migrate/20141023123456_change_hb_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeHbColumnName  < ActiveRecord::Migration
-  def change
-    rename_column :hooked_browsers, :type, :btype
-    rename_column :hooked_browsers, :version, :bversion
-  end
-end
