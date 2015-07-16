6bf9118c031143649196c3b22ba4da13049831da
Meatballs <eat_meatballs@hotmail.co.uk>
2015-07-17 00:29:17 +0100

    Remove redundant code

--- lib/phishing_frenzy/sidekiq_logger.rb
+++ lib/phishing_frenzy/sidekiq_logger.rb
@@ -3,15 +3,5 @@ module PhishingFrenzy
     def self.file_name_noext
       'sidekiq'
     end
-
-    def self.file_name
-      "#{file_name_noext}.log"
-    end
-
-    def self.read
-      path = Rails.root.join('log', file_name)
-      return '' unless File.exist?(path)
-      File::Tail::Logfile.open(path, backward: 2000)
-    end
   end
 end
