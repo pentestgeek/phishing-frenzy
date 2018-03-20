f7c66fdf3ddde14e9a81908715dcc07b09f6f0a5
zeknox <mccann.brandon@gmail.com>
2018-03-20 17:22:22 -0400

    added #template-files-help popover which provides additional documentation to users on the template#edit view when creating templates

--- app/assets/javascripts/templates.js
+++ app/assets/javascripts/templates.js
@@ -8,15 +8,19 @@
 $( document ).ready(function() {
   // enable dataTable on templates#list
   $('#templates').dataTable( {
-  "aoColumnDefs": [ { 'bSortable': false, "aTargets": [ 0, 4 ] } ],
-  "iDisplayLength": 25
+    "aoColumnDefs": [ { 'bSortable': false, "aTargets": [ 0, 4 ] } ],
+    "iDisplayLength": 25
   });
 
-$('#attachment_content').each(function() {
-         var editor = CodeMirror.fromTextArea(this, {
-             viewportMargin: Infinity,
-             lineNumbers : true,
-             matchBrackets : true
-         });
-     })
+  $('#attachment_content').each(function() {
+    var editor = CodeMirror.fromTextArea(this, {
+      viewportMargin: Infinity,
+      lineNumbers : true,
+      matchBrackets : true
+    });
+  })
+
+  $('[data-toggle="popover"]').popover({
+    html : true
+  });
 });
