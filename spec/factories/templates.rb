166e6c91f38939a9c4f91c3c3a0b535b719de446
Fernando Cabrera <ingfernandoc@gmail.com>
2023-11-07 14:52:42 -0500

    add functionality gems and rails update and refactor for factory class and view using devise errors for deprecation warning message

--- spec/factories/templates.rb
+++ spec/factories/templates.rb
@@ -18,6 +18,6 @@ FactoryBot.define do
   factory :template, :class => 'Template' do
     sequence(:name) {|n| "Template Name #{n}"}
     sequence(:description) {|n| "Template Description #{n}"}
-    directory_index "index.php"
+    directory_index {"index.php"}
   end
 end
